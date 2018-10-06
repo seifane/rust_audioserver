ec601ff95fe6205f44d5054664eed36522f7e483
Seïfane Idouchach <seifane.idouchach@epitech.eu>
2018-10-07 01:38:30 +0800

    Code refactoring

--- src/client/main.rs
+++ src/client/main.rs
@@ -16,27 +16,39 @@ use std::collections::VecDeque;
 extern crate sample;
 
 use sample::{signal, Signal};
-use sample::interpolate::{Converter, Floor, Linear};
+use sample::interpolate::{Converter, Linear};
 
+extern crate indicatif;
+use indicatif::ProgressBar;
 
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:34001").expect("Cannot bind");
-    socket.connect("127.0.0.1:3400").expect("Cannot connect");
-    
+    let buf_vec: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let net_buf = buf_vec.clone();
+
     let event_loop = EventLoop::new();
     let device = cpal::default_output_device().expect("No output devices");
     let mut supported_formats_range = device.supported_output_formats().expect("error formats");
     
     let format = supported_formats_range.next().expect("no formats").with_max_sample_rate();
 
+    let sample_rate = format.sample_rate.0;
+    let channels = format.channels;
+
+    println!("Sample_rate: {} | Channels: {}", sample_rate, channels);
+    
     let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
+    
+    sound_loop(event_loop, stream_id, format, net_buf);
+    socket_loop(buf_vec);
+}
+
+fn sound_loop(event_loop: EventLoop, stream_id: cpal::StreamId,
+              format: cpal::Format,
+              net_buf :Arc<Mutex<VecDeque<f32>>>) -> () {
     event_loop.play_stream(stream_id);
 
-    let mut buf_vec: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::new()));
-    let net_buf = buf_vec.clone();
-
     thread::spawn(move || {
-        while net_buf.lock().unwrap().len() < 30000 {
+        while net_buf.lock().unwrap().len() < 60000 {
             
         }
         event_loop.run(move |_stream_id, mut stream_data| {
@@ -51,17 +63,13 @@ fn main() {
                     println!("i:{}", i);
                 },
                 StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
-                    //                println!("bufferI16: {}", buffer.len());
-                    println!("net_buf len{}", net_buf.lock().unwrap().len());
-                    println!("sample rate{:?}, channels{:?}", format.sample_rate, format.channels);
                     for samp in buffer.chunks_mut(format.channels as usize) {
                         let mut val : i16 = 0;
                         for out in samp.iter_mut() {
                             let sample = net_buf.lock().unwrap().pop_front();
-                        if sample.is_none() == false {
-                            val = sample.unwrap().to_i16();
-                        }
-
+                            if sample.is_none() == false {
+                                val = sample.unwrap().to_i16();
+                            }
                             *out = val;
                         }
                     }
@@ -78,7 +86,13 @@ fn main() {
             }
         });
     });
+}
 
+fn socket_loop(buf_vec :Arc<Mutex<VecDeque<f32>>>) -> () {
+    let socket = UdpSocket::bind("127.0.0.1:34001").expect("Cannot bind");
+    socket.connect("127.0.0.1:3400").expect("Cannot connect");
+
+    let bar = ProgressBar::new(200000);
     
     loop {
         let mut buf = [0; 20000];
@@ -88,42 +102,22 @@ fn main() {
         let decoded : Vec<f32> = bincode::deserialize(&filled_buf[..]).unwrap();
         let mut vec_dec = Vec::new();
 
-        /*for chunk in decoded.chunks(2) {
-//            let arr = [chunk[0], chunk[1];
-            vec_dec.extend_from_slice(&[[chunk[0], chunk[1]]]);
-        }*/
-
-
         for d in decoded.iter() {
             vec_dec.extend_from_slice(&[[*d]]);
         }
-        
-        //        println!("decoded buffer {:?}", &decoded);
+
         let mut iter_signal = signal::from_iter(vec_dec.iter().cloned());
 
         let interp = Linear::from_source(&mut iter_signal);
-        //let frames = iter_signal.from_hz_to_hz(interp, 44_100 as f64, 192_000 as f64);
-
-        //        let frames = iter_signal.set_playback_hz_scale(interp, 0.25);
 
         let mut frames = Converter::scale_sample_hz(iter_signal, interp, 4 as f64);
         frames.set_hz_to_hz(44_100 as f64, 192_000 as f64);
-        
-       /* let ring_buffer = ring_buffer::Fixed::from([[0.0]; 100]);
-        let sinc = interpolate::Sinc::new(ring_buffer);
-        let new_signal = iter_signal.from_hz_to_hz(sinc, 44_100 as f64, 192_000 as f64);
-        */
+
         for frame in frames.until_exhausted() {
             for f in frame.iter() {
                 buf_vec.lock().unwrap().push_back(*f);                
             }
         }
-        
-/*        for s in vec_dec.iter() {
-            for i in 0..2 {
-                buf_vec.lock().unwrap().push_back(*s);
-            }
-        }*/
-//        buf_vec.lock().unwrap().append(&mut vec_dec);
-    }
+        bar.set_position(buf_vec.lock().unwrap().len() as u64);
+    }    
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -4,11 +4,9 @@ use std::thread;
 
 extern crate bincode;
 
-use std::f64::consts::PI;
-
 extern crate simplemad;
 
-use simplemad::{Decoder, Frame};
+use simplemad::Decoder;
 use std::fs::File;
 use std::path::Path;
 
@@ -20,15 +18,49 @@ extern crate ratelimit_meter;
 
 use ratelimit_meter::{LeakyBucket, Decider};
 
+extern crate rustyline;
+
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:3400").expect("couldn't bind to address");
+    let buf_vec: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::new()));
+    //let buf_vec_clone = buf_vec.clone();
 
-    let mut buf_vec: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::new()));
-    let buf_vec_clone = buf_vec.clone();
-    
+//    decode_mp3("test.mp3", buf_vec.clone());
+    socket_loop(buf_vec.clone());
+
+    let mut rl = rustyline::Editor::<()>::new();
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let boxed_line = Box::new(line);
+                let parsed = Box::leak(boxed_line).split_whitespace();
+                let mut parsed_vec = Vec::new();
+
+                for p in parsed {
+                    parsed_vec.push(p);
+                }
+                
+                if parsed_vec[0] == "load" && parsed_vec.len() == 2 {
+                    decode_mp3(parsed_vec[1], buf_vec.clone());
+                }
+                if parsed_vec[0] == "clear" {
+                    buf_vec.lock().unwrap().clear();
+                }
+            },
+            Err(_) => std::process::exit(0),
+        }
+      // std::thread::sleep_ms(100);
+    }
+}
+
+fn decode_mp3(filepath: &'static str, buf_vec_clone: Arc<Mutex<VecDeque<f32>>>) -> () {
     thread::spawn(move || {
-
-        let path = Path::new("test.mp3");
+        println!("Spawning thread to parse mp3 at {}", filepath);
+        let path = Path::new(filepath);
+        if !path.exists() {
+            println!("Path doesn't exist");
+            return ();
+        }
         let file = File::open(&path).unwrap();
         let decoder = Decoder::decode(file).unwrap();
     
@@ -41,10 +73,12 @@ fn main() {
                         buffer_left.push(sample.to_f32());
                     }
                     let mut buffer_right = Vec::new();
-                    for sample in frame.samples[1].iter() {
-                        buffer_right.push(sample.to_f32());
+                    if frame.samples.len() > 1 {
+                        for sample in frame.samples[1].iter() {
+                            buffer_right.push(sample.to_f32());
+                        }
                     }
-                    
+
                     let mut final_buffer = Vec::new();
                     if buffer_right.len() == buffer_left.len() {
                         for i in 0..buffer_right.len() {
@@ -57,126 +91,43 @@ fn main() {
                             final_buffer.push(buffer_left[i]);
                         }
                     }
-                    println!("Frame sample rate: {} left sample size:{}",
-                             frame.sample_rate, buffer_left.len());
+                   
+                    /*println!("Frame sample rate: {} left sample size:{}",
+                             frame.sample_rate, buffer_left.len());*/
 
                     buf_vec_clone.lock().unwrap().append(&mut buffer_left.into_iter().collect());
                 }
             }
         }
+        println!("Done importing mp3");
     });
+}
 
-    let mut lb: LeakyBucket = LeakyBucket::per_second(20).unwrap(); //2205 samples at a time;
-    
-    loop {
-        if buf_vec.lock().unwrap().len() >= 2205 && lb.check() == Ok(()) {
-            let mut to_send : Vec<f32> = Vec::new();
-
-            for i in 0..2205 {
-                to_send.push(buf_vec.lock().unwrap().pop_front().unwrap());
+fn socket_loop(buf_vec :Arc<Mutex<VecDeque<f32>>>) -> () {
+    thread::spawn(move || {
+        let socket = UdpSocket::bind("0.0.0.0:3400").expect("couldn't bind to address");
+        
+        let mut lb: LeakyBucket = LeakyBucket::per_second(20).unwrap(); //2205 samples at a time;
+        
+        loop {
+            if lb.check() == Ok(()) {
+                let mut to_send : Vec<f32> = Vec::new();
+                
+                for _i in 0..2205 {
+                    if buf_vec.lock().unwrap().len() > 0 {
+                        to_send.push(buf_vec.lock().unwrap().pop_front().unwrap());
+                    } else {
+                        to_send.push(0 as f32);
+                    }
+                }
+                let ser = bincode::serialize(&to_send).expect("Cannot serialize");
+                
+                socket.send_to(&ser, "127.0.0.1:34001").expect("Cannot send to client");
+                socket.send_to(&ser, "192.168.0.100:34001").expect("Cannot send to client");
+         //       println!("Sent frame with {} bytes", ser.len());
+            } else {
+                std::thread::sleep_ms(10);
             }
-            let ser = bincode::serialize(&to_send).expect("Cannot serialize");
-            
-            socket.send_to(&ser, "127.0.0.1:34001");
-            println!("Sent frame with {} bytes", ser.len());
-        } else {
-            std::thread::sleep_ms(10);
         }
-
-    }
+    });
 }
-
-
-/*extern crate portaudio;
-
-use portaudio as pa;
-use std::f64::consts::PI;
-
-const CHANNELS: i32 = 2;
-const NUM_SECONDS: i32 = 1;
-const SAMPLE_RATE: f64 = 44_100.0;
-const FRAMES_PER_BUFFER: u32 = 1024;
-const TABLE_SIZE: usize = 200;
-
-extern crate tokio;
-extern crate tokio_codec;
-extern crate tokio_io;
-
-use std::net::SocketAddr;
-use tokio::prelude::*;
-use tokio::net::{UdpSocket, UdpFramed};
-use tokio_codec::BytesCodec;
-
-fn main() {
-    println!("Hello, world!");
-
-    let addr: SocketAddr = "127.0.0.1:3400".parse().unwrap();
-    let socket = UdpSocket::bind(&addr).unwrap();
-    let (socketSink, socketStream) = UdpFramed::new(socket, BytesCodec::new()).split();
-
-    tokio::run(socket);
-    
-    match run(&socketSink) {
-        Ok(_) => {
-            println!("Everything fine");
-        },
-        e => {
-            eprintln!("Error");
-        }
-    }
-}
-
-fn run(socketSink: &tokio::prelude::Sink) -> Result<(), pa::Error> {
-
-    let mut sine = [0.0; TABLE_SIZE];
-    for i in 0..TABLE_SIZE {
-        sine[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
-    }
-
-    let mut left_phase = 0;
-    let mut right_phase = 0;
-
-    let mut totalFrames = 0;
-
-    println!("sine:{}", sine);
-    
-    let pa = try!(pa::PortAudio::new());
-    let mut settings = try!(pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
-    settings.flags = pa::stream_flags::CLIP_OFF;
-
-    let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
-        let mut idx = 0;
-
-        totalFrames += frames;
-        
-        println!("length {}", totalFrames);
-        
-        for _ in 0..frames {
-            buffer[idx]   = sine[left_phase];
-            buffer[idx+1] = sine[right_phase];
-            left_phase += 1;
-            if left_phase >= TABLE_SIZE { left_phase -= TABLE_SIZE; }
-            right_phase += 4;
-            if right_phase >= TABLE_SIZE { right_phase -= TABLE_SIZE; }
-            idx += 2;
-        }
-
-        socketSink.send_all(buffer);
-        pa::Continue
-    };
-
-    let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
-
-    try!(stream.start());
-
-    println!("Play for {} seconds.", NUM_SECONDS);
-    pa.sleep(NUM_SECONDS * 1_000);
-
-    try!(stream.stop());
-    try!(stream.close());
-
-    println!("Test finished.");
-
-    Ok(())
-}
-*/
